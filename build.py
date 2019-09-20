a7f7a25fd6292594e4ab2b9268568a1ae84e4d7a
Devin Schwab <digidevin@gmail.com>
2019-09-20 14:32:13 -0400

    :construction_worker: Setup named pages and changed build types

--- build.py
+++ build.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python
 import copy
+from collections import defaultdict
 
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
-
+    builder.add_common_builds(shared_option_name="magnum-plugins:build_plugins_static")
+    
     # add c++17 build configs
-    new_builds = []
+    named_builds = defaultdict(list)
     for settings, options, env_vars, build_requires, reference in builder.items:
-        new_settings = copy.copy(settings)
-        new_settings["compiler.cppstd"] = "17"
-        new_settings["compiler.libcxx"] = "libstdc++11"
-        new_options = copy.copy(options)
-        new_options["assimp:build_zlib"] = False
-        new_builds.append([new_settings, options, env_vars, build_requires])
-        new_builds.append([new_settings, new_options, env_vars, build_requires])
-    builder.builds = new_builds
+        settings["compiler.cppstd"] = "17"
+        settings["compiler.libcxx"] = "libstdc++11"
+        options["magnum-plugins:with_assimpimporter"] = False
+        options["magnum-plugins:with_freetypefont"] = False
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'static' if options['magnum-plugins:build_plugins_static'] else 'shared'}"
+        ].append([settings, options, env_vars, build_requires, reference])
+
+        assimp_options = copy.copy(options)
+        assimp_options["with_assimpimporter"] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'static' if options['magnum-plugins:build_plugins_static'] else 'shared'}-assimp"
+        ].append([settings, assimp_options, env_vars, build_requires, reference])
+
+        freetype_options = copy.copy(options)
+        freetype_options["with_freetypefont"] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'static' if options['magnum-plugins:build_plugins_static'] else 'shared'}-freetype"
+        ].append([settings, freetype_options, env_vars, build_requires, reference])
+
+        assimp_and_freetype_options = copy.copy(options)
+        assimp_and_freetype_options['magnum-plugins:with_assimpimporter'] = True
+        assimp_and_freetype_options['magnum-plugins:with_freetypefont'] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'static' if options['magnum-plugins:build_plugins_static'] else 'shared'}"
+            "-assimp-freetype"
+        ].append(
+            [settings, assimp_and_freetype_options, env_vars, build_requires, reference]
+        )
+
+    builder.builds = []
+    builder.named_builds = named_builds
 
     builder.run()
